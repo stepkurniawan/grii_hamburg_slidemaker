ea35b2571d53ab3089d15a32dcb4d9d727db8e2b
Stephen Kurniawan <stepkurniawan@gmail.com>
2023-07-17 17:46:47 +0200

    done bible verse

--- main.py
+++ main.py
@@ -67,7 +67,10 @@ from pptx.enum.dml import MSO_FILL
 # 1. 4 song's numbers
 SONG_NUMBERS = []
 # 2. Opening Bible verse
-OPEN_BIBLE_VERSE = ""
+OPEN_BIBLE_BOOK = ""
+OPEN_BIBLE_CHAPTER = ""
+OPEN_BIBLE_VERSE_START = ""
+OPEN_BIBLE_VERSE_END = ""
 # 3. Pastor's title in Indonesian and German, and name
 PASTOR_TITLE_ID = ""
 PASTOR_TITLE_DE = ""
@@ -87,6 +90,11 @@ TEMPLATE_FILE = 'master_slide_template.pptx'
 ########################################### Functions ##########################################
 
 def main():
+
+    """
+    the structure of the slide is
+    Presentation -> Layout name -> slide layout -> slide -> shapes -> placeholders
+    """
     
     # create a test presentation file
     prs = Presentation(TEMPLATE_FILE)
@@ -112,7 +120,7 @@ def main():
     add_church_cover_page(prs)
 
     check_placeholders_in_slide_index(prs, 4)
-    add_bibel_reading_page(prs)
+    add_bible_reading_page(prs)
 
     # save file
     prs.save('test.pptx')
@@ -130,10 +138,9 @@ def insert_slides_from_pict_folder(prs, folder_path):
         file_path = os.path.join(folder_path, file)
         pic = slide.shapes.add_picture(file_path, Inches(0), Inches(0), width=prs.slide_width, height=prs.slide_height)
 
-
-
-def add_static_page(prs, layout_name):
-    # Find the layout with the specified name
+def get_slide_layout_from_layout_name(prs,layout_name):
+    
+    # Specify the layout name you want to use
     slide_layout = None
     for layout in prs.slide_master.slide_layouts:
         if layout.name == layout_name:
@@ -150,27 +157,39 @@ def add_static_page(prs, layout_name):
 def add_beginning_slide(prs):
     # Specify the layout name you want to use
     layout_name = "beginning" # renamed in the master template pptx file
-    add_static_page(prs, layout_name)
+    get_slide_layout_from_layout_name(prs, layout_name)
 
 
 
 def add_church_cover_page(prs):
     # Specify the layout name you want to use
     layout_name = "church_cover" # renamed in the master template pptx file
-    add_static_page(prs, layout_name)
+    get_slide_layout_from_layout_name(prs, layout_name)
 
 
 
-def add_bibel_reading_page(prs):
+def add_bible_reading_page(prs):
     # Find the layout with the specified name
-    slide_layout = prs.slide_master.slide_layouts[4]
-
-    slide = prs.slides.add_slide(slide_layout)
+    layout_name_cover = "BIBLE_READING" # renamed in the master template pptx file
+    slide_layout_cover = get_slide_layout_from_layout_name(prs, layout_name_cover)
     try: 
-        bible_verse = slide.placeholders[10]
-        bible_verse.text = "Jesaya 55:6-11"
+        # check_placeholders_in_slide(prs,slide_layout_cover)
+        bible_verse = slide_layout_cover.placeholders[10]
+        bible_verse.text = "Jesaya 55:6-11" #TOOD: get from input	
     except IndexError:
         print("Invalid placeholder index.")
+
+
+    bible_verse_layout_name = "BIBLE_VERSE" # renamed in the master template pptx file
+    slide_layout_bible_verse = get_slide_layout_from_layout_name(prs, bible_verse_layout_name)
+    try:
+        print("bible verse layout")
+        check_placeholders_in_slide(prs,slide_layout_bible_verse)
+        
+    except IndexError:
+        print("Invalid placeholder index.")
+    
+    
 
 
 ######## Helper functions ####################################################
@@ -185,7 +204,7 @@ def check_placeholders_in_slide_index(prs, layout_index):
     print('check done. Remember, the id is more important than the position')
 
 def check_placeholders_in_slide(prs, slide):
-    print('in slide %d :' % slide)
+    print('in slide %s' % slide)
     for shape in slide.placeholders:
         if shape.is_placeholder:
             phf = shape.placeholder_format
