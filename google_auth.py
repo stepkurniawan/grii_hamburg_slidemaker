7bf050a15f24a5dae1998c84beb62a7272b289f3
Stephen Kurniawan <stepkurniawan@gmail.com>
2023-07-30 22:16:03 +0200

    using service account on streamlit

--- google_auth.py
+++ google_auth.py
@@ -12,6 +12,11 @@ from google_auth_oauthlib.flow import InstalledAppFlow # missing refresh_token
 from google.auth.transport.requests import Request
 from googleapiclient.errors import HttpError
 
+from streamlit_oauth import OAuth2Component # pip install streamlit-oauth
+
+from google.oauth2 import service_account
+
+
 # pip install python-dotenv
 from dotenv import load_dotenv
 load_dotenv()
@@ -49,6 +54,9 @@ def test_connection(creds):
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
+    try_build_drive_service(creds)
+
+def try_build_drive_service(creds):
     try:
         service = build('drive', 'v3', credentials=creds)
 
@@ -66,6 +74,7 @@ def test_connection(creds):
     except HttpError as error:
         # TODO(developer) - Handle errors from drive API.
         print(f'An error occurred: {error}')
+
 
 def test_connection_using_TOML_file(creds):
     """
@@ -100,23 +109,7 @@ def test_connection_using_TOML_file(creds):
                 creds = flow.run_local_server(port=0)
 
 
-    try:
-        service = build('drive', 'v3', credentials=creds)
-
-        # Call the Drive v3 API
-        results = service.files().list(
-            pageSize=10, fields="nextPageToken, files(id, name)").execute()
-        items = results.get('files', [])
-
-        if not items:
-            print('No files found.')
-            return
-        print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
-    except HttpError as error:
-        # TODO(developer) - Handle errors from drive API.
-        print(f'An error occurred: {error}')
+    try_build_drive_service(creds)
 
 def test_connection_using_streamlit_secrets(creds):
     """
@@ -158,21 +151,20 @@ def test_connection_using_streamlit_secrets(creds):
             creds = flow.run_local_server(port=0)
 
 
-    try:
-        service = build('drive', 'v3', credentials=creds)
+    try_build_drive_service(creds)
 
-        # Call the Drive v3 API
-        results = service.files().list(
-            pageSize=10, fields="nextPageToken, files(id, name)").execute()
-        items = results.get('files', [])
-
-        if not items:
-            print('No files found.')
-            return
-        print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
-    except HttpError as error:
-        # TODO(developer) - Handle errors from drive API.
-        print(f'An error occurred: {error}')
+def connect_service_account_streamlit(creds):
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            creds = service_account.Credentials.from_service_account_info(
+                st.secrets["gcp_service_account"],
+                scopes=SCOPES,
+            )
+    
+    try_build_drive_service(creds)
 
